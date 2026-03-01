5aea0b8d0b67c970ac4d066a8a766afa6622bcf4
JubeirMorad <jubiermorad@gmail.com>
2026-03-01 09:47:17 +0200

    remove record (GetMemberShipByIdQuery) add (GetMemberShipByUserAndProjectId) in memberShipsQueries

--- ProjectsManagementStudio.Application/Features/MemberShips/Queries/MemberShipQueries.cs
+++ ProjectsManagementStudio.Application/Features/MemberShips/Queries/MemberShipQueries.cs
@@ -5,18 +5,20 @@ using System.Threading.Tasks;
 
 namespace ProjectsManagementStudio.Application.Features
 {
-    public record GetMemberShipByIdQuery
-    (
-        Guid Id
-    );
 
     public record GetMemberShipsByProjectIdQuery
     (
         Guid ProjectId
     );
 
-     public record GetMemberShipsByUserIdQuery
+    public record GetMemberShipsByUserIdQuery
     (
+        Guid UserId
+    );
+
+    public record GetMemberShipByUserAndProjectIdQuery
+    (
+        Guid ProjectId,
         Guid UserId
     );
 }
